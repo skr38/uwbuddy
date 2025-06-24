9c8bac0859de6b1d56e4bf8320e928653e463e41
Sebastian Krolo <s.krolo@hm.edu>
2025-06-24 17:26:53 +0200

    changed IP for stupid people

--- orchestrate/main_mqtt.py
+++ orchestrate/main_mqtt.py
@@ -30,7 +30,7 @@ def process_orientation(angular_velocity_data):
 
 
 # Configuration
-BROKER = "10.19.71.27"
+BROKER = "10.19.71.5"
 PORT = 1883
 TOPIC = "dwm/node/+/uplink/location"
 TUMBLLER_NODE_ID = "4c87"
